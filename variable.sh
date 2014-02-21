2210701c09929bb2228980e18610afe6e6f75cda
kosl90 <kos1990l@gmail.com>
2014-02-21 15:10:20 +0800

    tmux add new binding, a little for zsh.
    1. tmux add PREFIX-/ to create new session
    2. add some new environment variables to zsh.
    3. add go plugins to zsh.

--- variable.sh
+++ variable.sh
@@ -10,15 +10,15 @@ export EDITOR=vim
 export GAE_PATH=$HOME/GAE
 export GOAGENT_PATH=$GAE_PATH/goagent
 
-# export http_proxy="http://localhost:8087"
-
 export DISPLAY=:0
+
+export GOPATH=$HOME/Workspace/go
 
 PATH=/usr/lib/lightdm/lightdm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
 PATH=$PATH:$TOOLS/apache-maven/bin:$HOME/.cabal/bin:/usr/local/go/bin:$HOME/src/scripts
 PATH=$PATH:/opt/QtSDK/Desktop/Qt/4.8.1/gcc/bin:/opt/QtSDK/QtCreator/bin:/usr/local/sml/bin
 PATH=$PATH:$GAE_PATH:$HOME/node_modules/.bin/:$QT5PATH/bin/
-PATH=$PATH:$HOME/.local/bin
+PATH=$PATH:$HOME/.local/bin:$GOPATH/bin:/usr/lib/go/bin
 
 # to fix tmux connect dbus failed
 # unset DBUS_SESSION_BUS_ADDRESS
