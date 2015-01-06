c3860ae5fce83c85116370a30fb68c4ce5a7e52a
liliqiang <liliqiang@linuxdeepin.com>
2015-01-06 10:52:04 +0800

    update git and shell env variables

--- variable.sh
+++ variable.sh
@@ -3,10 +3,11 @@ export TOOLS=$SRC/tools
 export MYWIKI=$HOME/mywiki
 export WIKIDATA=$MYWIKI/wikidata
 export NOTES=$HOME/Dropbox/notes
+export WEBKIT_INSPECTOR_PATH=$HOME/webinspector
 
 export QT5PATH="/opt/Qt5.1.1/5.1.1/gcc/"
 
-export WORKSPACE=$HOME/workspace
+export WORKSPACE=$HOME/Workspace
 export EDITOR=vim
 export GAE_PATH=$HOME/GAE
 export GOAGENT_PATH=$GAE_PATH/goagent
@@ -22,11 +23,14 @@ export GOPATH=$MY_GOPATH:$DEEPIN_GOPATH:$SYS_GOPATH
 export SBT_OPTS="${SBT_OPTS} -DsocksProxyHost=127.0.0.1 -DsocksProxyPort=7070"
 #export SBT_OPTS="${SBT_OPTS} -DhttpProxy.Host=127.0.0.1 -DhttoProxy.Port=7070"
 
+export DDE=$WORKSPACE/deepin/dde-workspace
+export DAEMON=$WORKSPACE/deepin/gohome/src/pkg.linuxdeepin.com/dde-daemon/
+
 PATH=/usr/lib/lightdm/lightdm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
 PATH=$PATH:$TOOLS/apache-maven/bin:$HOME/.cabal/bin:/usr/local/go/bin:$HOME/src/scripts
 PATH=$PATH:/opt/QtSDK/Desktop/Qt/4.8.1/gcc/bin:/opt/QtSDK/QtCreator/bin:/usr/local/sml/bin
 PATH=$PATH:$GAE_PATH:$HOME/node_modules/.bin/:$QT5PATH/bin/
-PATH=$PATH:$HOME/.local/bin:$MY_GOPATH/bin:/usr/lib/go/bin:$GOPATH/bin
+PATH=$PATH:$HOME/.local/bin:$MY_GOPATH/bin:/usr/lib/go/bin:$GOPATH/bin:$DEEPIN_GOPATH/bin
 PATH=$QT5PATH/bin:$PATH
 
 # to fix tmux connect dbus failed
