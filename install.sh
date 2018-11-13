4d41b6602b06c8d79fed951431707de2c8e016f2
kosl90 <kos1990l@gmail.com>
2018-11-13 10:16:43 +0800

    update: adapt installation for mac

--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
-OS=`grep -w ID /etc/os-release | cut -d= -f2`
+if [ -f /etc/os-release ]; then
+    OS=`grep -w ID /etc/os-release | cut -d= -f2`
+else
+    OS=`uname`
+fi
+
 echo "OS: $OS"
 
 if [ X"$OS" = X"arch" ]
 then
     INS="pacman -S"
+elif [ X"$OS" = X"Darwin" ]; then
+    echo 'not support mac now'
+    # TODO: not support now
+    # INS="brew install"
 else
     INS="apt-get install"
 fi
 
 #echo $INS
 
-if [ ! $USER = "root" ]
+if [ ! $USER = "root" ] && [ X$INS != X"" ];
 then
     INS="sudo $INS"
 fi
