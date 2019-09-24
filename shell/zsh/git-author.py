62220ac2bfeb868d31d6ff11e1845e65db7fe8ff
kosl90 <kos1990l@gmail.com>
2019-09-24 15:07:50 +0800

    update(zsh): auto change git author
    auto change git author according to the pwd.
    
    configures ~/.config/gitauthor.json with format:
    
    ~~~json
    [
      {
        path_prefix: "",
        name: "",
        email: ""
      }
    ]
    ~~~
    
    If there is no configuration found, the git config will be used. Otherwise,
    environment variables GIT_AUTHOR_NAME, GIT_AUTHOR_EMAIL, GIT_COMMITTER_NAME and
    GIT_COMMITTER_EMAIL will be set according to the first found configuration.
    
    Note: environment variables is set, but `git config --get user.name` will still
    output the configuration value. Please use `git var GIT_AUTHOR_IDENT` or
    `git var GIT_COMMITTER_IDENT` to see the value.

---
+++ shell/zsh/git-author.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import os
+import sys
+
+CONFIG_FILE = os.path.expandvars('$HOME/.config/gitauthor.json')
+
+def outputShell(name, email):
+    print('export GIT_AUTHOR_NAME={}'.format(name));
+    print('export GIT_AUTHOR_EMAIL={}'.format(email));
+    print('export GIT_COMMITTER_NAME={}'.format(name));
+    print('export GIT_COMMITTER_EMAIL={}'.format(email));
+
+def readConfig(f):
+    import json
+    with open(f) as config:
+        return json.load(config)
+
+def findAuthorConfig(projectPath, config):
+    for item in iter(config):
+        if projectPath.startswith(item['path_prefix']):
+            return item
+    return None
+
+def main(projectPath):
+    if not os.path.exists(CONFIG_FILE):
+        sys.exit(1)
+
+    c = readConfig(CONFIG_FILE);
+
+    firstGitAuthorConfig = findAuthorConfig(projectPath, c)
+
+    if firstGitAuthorConfig is not None:
+        outputShell(firstGitAuthorConfig['name'], firstGitAuthorConfig['email'])
+
+if __name__ == '__main__':
+    main(os.environ['PWD'])
