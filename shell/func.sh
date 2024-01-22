2ea433a883d77a98b1abd8e9808e5d03b4735e73
kosl90 <kos1990l@gmail.com>
2024-01-22 20:30:33 +0800

    feat: update useClash/useNoClash
    support GLOBAL_AGENT_HTTP_PROXY/GLOBAL_AGENT_HTTPS_PROXY

--- shell/func.sh
+++ shell/func.sh
@@ -165,11 +165,11 @@ function findRcPackages() {
 }
 
 function useClash() {
-    export HTTPS_PROXY=http://127.0.0.1:7890 HTTP_PROXY=http://127.0.0.1:7890 ALL_PROXY=socks5://127.0.0.1:7890
+    export HTTPS_PROXY=http://127.0.0.1:7890 HTTP_PROXY=http://127.0.0.1:7890 ALL_PROXY=socks5://127.0.0.1:7890 GLOBAL_AGENT_HTTP_PROXY=http://127.0.0.1:7890 GLOBAL_AGENT_HTTPS_PROXY=http://127.0.0.1:7890
 }
 
 function useNoClash() {
-    export HTTP_PROXY= HTTPS_PROXY ALL_PROXY=
+    export HTTP_PROXY= HTTPS_PROXY= ALL_PROXY= GLOBAL_AGENT_HTTP_PROXY= GLOBAL_AGENT_HTTPS_PROXY=
 }
 
 function useNoProxy() {
